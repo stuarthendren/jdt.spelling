1d84173fafee303cfe76c49fc8d0e0dc7c55c15f
Stuart Hendren <stuarthendren@gmail.com>
2013-09-07 12:59:18 +0100

    Add content assist shell
    Not currently sure if we need both the MarkerResolutionGenerator and the QuickAssistProcessor

---
+++ plugins/jspell/src/jspell/resolution/JSpellMarkerResolutionGenerator.java
@@ -0,0 +1,15 @@
+package jspell.resolution;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.ui.IMarkerResolution;
+import org.eclipse.ui.IMarkerResolutionGenerator;
+
+public class JSpellMarkerResolutionGenerator implements IMarkerResolutionGenerator {
+
+	@Override
+	public IMarkerResolution[] getResolutions(IMarker marker) {
+		// TODO Auto-generated method stub
+		return new IMarkerResolution[0];
+	}
+
+}
